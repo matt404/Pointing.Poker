f6c339deb874ee34f5d546a5f013328a4f48cb51
Matt Wilson <matt.wilson@workflowintegrations.com>
2015-06-22 07:26:56 -0700

    initial check in

---
+++ src/java/pointingpoker/websocket/MemberSessionHandler.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pointingpoker.websocket;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.JsonObject;
+import javax.json.spi.JsonProvider;
+import javax.websocket.Session;
+import pointingpoker.model.Member;
+
+@ApplicationScoped
+public class MemberSessionHandler {
+
+	private int memberId = 0;
+//	private final Set<Session> sessions = new HashSet();
+//	private final Set<Member> members = new HashSet();
+	private final Map<String, Set<Session>> sessions = new HashMap<>();
+	private final Map<String, Set<Member>> members = new HashMap<>();
+
+	public void addSession(Session session, String roomKey) {
+		if(!sessions.containsKey(roomKey)){
+			sessions.put(roomKey, new HashSet());
+		}
+		sessions.get(roomKey).add(session);
+		if(members.containsKey(roomKey)){
+			for (Member member : members.get(roomKey)) {
+				JsonObject addMessage = createAddMessage(member);
+				sendToSession(session, addMessage, roomKey);
+			}
+		}
+	}
+
+	public void addMember(Member member, String roomKey) {
+		if(!members.containsKey(roomKey)){
+			members.put(roomKey, new HashSet());
+		}
+		member.setId(memberId);
+		members.get(roomKey).add(member);
+		memberId++;
+		JsonObject addMessage = createAddMessage(member);
+		sendToAllConnectedSessions(addMessage, roomKey);
+//		if(!member.getObserver()){
+//			startNewGame();
+//		}
+	}
+
+	private Member getMemberById(int id, String roomKey) {
+		if(members.containsKey(roomKey)){
+			for (Member member : members.get(roomKey)) {
+				if (member.getId() == id) {
+					return member;
+				}
+			}
+		}
+		return null;
+	}
+
+	private JsonObject createAddMessage(Member member) {
+		JsonProvider provider = JsonProvider.provider();
+		JsonObject addMessage = provider.createObjectBuilder()
+				.add("action", "add")
+				.add("id", member.getId())
+				.add("name", member.getName())
+				.add("vote", member.getVote())
+				.add("clientKey", member.getClientKey())
+				.add("observer", member.getObserver())
+				.build();
+		return addMessage;
+	}
+
+	public void removeMemberByClientKey(int clientKey, String roomKey) {
+		for (Member member : members.get(roomKey)) {
+			if(member.getClientKey().equals(clientKey)){
+				removeMember(member.getId(), roomKey);
+			}
+		}		
+	}
+	
+	public void removeMember(int id, String roomKey) {
+		Member member = getMemberById(id, roomKey);
+		if (member != null) {
+			members.get(roomKey).remove(member);
+			JsonProvider provider = JsonProvider.provider();
+			JsonObject removeMessage = provider.createObjectBuilder()
+					.add("action", "remove")
+					.add("id", id)
+					.build();
+			sendToAllConnectedSessions(removeMessage, roomKey);
+		}
+	}
+
+    public void removeSession(Session session, String roomKey) {
+		if(sessions.containsKey(roomKey)){
+			sessions.get(roomKey).remove(session);
+		}
+    }
+	
+	private void sendToAllConnectedSessions(JsonObject message, String roomKey) {
+		if(sessions.containsKey(roomKey)){
+			for (Session session : sessions.get(roomKey)) {
+				sendToSession(session, message, roomKey);
+			}
+		}
+	}
+
+	private void sendToSession(Session session, JsonObject message, String roomKey) {
+		try {
+			session.getBasicRemote().sendText(message.toString());
+		} catch (IOException ex) {
+			if(sessions.containsKey(roomKey)){
+				sessions.get(roomKey).remove(session);
+			}
+			Logger.getLogger(MemberSessionHandler.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public void startNewGame(String roomKey) {
+		for (Member member : members.get(roomKey)) {
+			member.setVote("");
+		}
+		JsonProvider provider = JsonProvider.provider();
+		JsonObject newGameMessage = provider.createObjectBuilder()
+				.add("action", "newgame")
+				.build();
+		sendToAllConnectedSessions(newGameMessage, roomKey);
+	}
+
+	public void submitVote(int id, String vote, String roomKey) {
+		JsonProvider provider = JsonProvider.provider();
+		Member member = getMemberById(id, roomKey);
+		if (member != null) {
+			member.setVote(vote);
+			JsonObject updateDevMessage = provider.createObjectBuilder()
+					.add("action", "vote")
+					.add("id", member.getId())
+					.add("vote", vote)
+					.build();
+			sendToAllConnectedSessions(updateDevMessage, roomKey);
+		}
+		checkForGameComplete(roomKey);
+	}
+
+	public void checkForGameComplete(String roomKey) {
+		Boolean gameIsComplete = true;
+
+		for (Member member : members.get(roomKey)) {
+			if(!member.getObserver() && member.getVote().isEmpty()){
+				gameIsComplete = false;
+				break;
+			}
+		}
+		
+		if (gameIsComplete) {
+			JsonProvider provider = JsonProvider.provider();
+			JsonObject showCardsMessage = provider.createObjectBuilder()
+					.add("action", "showcards")
+					.build();
+			sendToAllConnectedSessions(showCardsMessage, roomKey);
+		}
+	}
+
+	public List getMembers(String roomKey) {
+		return new ArrayList<>(members.get(roomKey));
+	}
+
+}
---
+++ web/index.js
@@ -0,0 +1,217 @@
+
+var PointingPoker = function(){
+	
+	var _socket, _clientKey, _roomKey;
+	
+	var init = function (){
+		window.onload = load;
+		window.onunload = this.exitRoom;
+	};
+	
+	var load = function(){
+		var savedRoomKey = localStorage.getItem('pointingpoker:roomkey');
+		var username = localStorage.getItem('pointingpoker:username');
+		if(typeof(savedRoomKey) === "string" && savedRoomKey !== ""){
+			document.getElementById("inputRoomKey").value = savedRoomKey;
+			document.getElementById("inputName").focus();
+			if(typeof(username) === "string" && username !== ""){
+				document.getElementById("inputName").value = username;
+				document.getElementById("inputName").select();		
+			}
+		}else{
+			document.getElementById("inputRoomKey").focus();			
+		}
+	};
+
+	var getWebSocketPath = function (){
+		var loc = window.location, newUri;
+		if (loc.protocol === "https:") {
+			newUri = "wss:";
+		} else {
+			newUri = "ws:";
+		}
+		newUri += "//" + loc.host;
+		newUri += loc.pathname + "member/actions";
+		return newUri;
+	};
+
+	var onMessage = function (event) {
+//		console.log(event);
+		var member = JSON.parse(event.data);
+		if (member.action === "add") {
+			printMemberElement(member);
+			if(sessionStorage.getItem('pointingpoker:clientkey')*1 === member.clientKey){
+				sessionStorage.setItem('pointingpoker:serverid', member.id);
+				PointingPoker.hideForm(member.observer);
+			}
+		}
+		if (member.action === "remove") {
+			document.getElementById("memberSpan"+member.id).remove();
+			document.getElementById("memberCardDiv"+member.id).remove();
+		}
+		if (member.action === "vote") {
+			var memberCardDiv = document.getElementById("memberCardDiv"+member.id);
+			memberCardDiv.setAttribute('votevalue',member.vote);
+			var memberSpan = document.getElementById("memberSpan"+member.id);
+			memberSpan.setAttribute("class", "member-tag label label-primary ticked");
+		}
+		if (member.action === "showcards") {
+			var resultsContainer = document.getElementById('resultsContainer');
+			var memberContainer = document.getElementById('memberContainer');
+			var pointPickerContainer = document.getElementById('pointPickerContainer');
+			var iCount = resultsContainer.childNodes.length;
+			for(var i=0; i < iCount; i++){
+				var result = resultsContainer.childNodes[i];
+				result.innerHTML = result.getAttribute("votevalue");
+				result.setAttribute("class", "card-result showValue");
+			}		
+		}
+		if (member.action === "newgame") {
+			var resultsContainer = document.getElementById('resultsContainer');
+			var memberContainer = document.getElementById('memberContainer');
+			var pointPickerContainer = document.getElementById('pointPickerContainer');
+			var iCount = resultsContainer.childNodes.length;
+			for(var i=0; i < iCount; i++){
+				resultsContainer.childNodes[i].innerHTML = "?";
+				resultsContainer.childNodes[i].setAttribute("class", "card-result hideValue");
+			}
+			iCount = pointPickerContainer.childNodes.length;
+			for(var i=0; i < iCount; i++){
+				pointPickerContainer.childNodes[i].className = "card";
+			}
+			iCount = memberContainer.childNodes.length;
+			for(var i=0; i < iCount; i++){
+				if(memberContainer.childNodes[i].getAttribute("isobserver") !== "true"){
+					memberContainer.childNodes[i].setAttribute("class", "member-tag label label-primary");
+				}
+			}
+		}
+	};
+
+	var printMemberElement = function (member) {
+		var memberContainer = document.getElementById("memberContainer");
+		var memberSpan = document.createElement("span");
+		memberSpan.id = "memberSpan"+member.id;
+		if(member.observer === true){
+			memberSpan.setAttribute("class", "member-tag label label-default");
+		}else{
+			memberSpan.setAttribute("class", "member-tag label label-primary");
+
+			var resultsContainer = document.getElementById("resultsContainer");
+			var memberCardDiv = document.createElement("div");
+			memberCardDiv.id = "memberCardDiv"+member.id;
+			memberCardDiv.setAttribute("class", "card-result hideValue");
+			memberCardDiv.innerHTML = "?";
+			if(member.vote !== ""){
+				memberCardDiv.setAttribute("votevalue", member.vote);
+			}
+			resultsContainer.appendChild(memberCardDiv);
+
+		}
+		memberSpan.setAttribute("isobserver", member.observer);
+		memberSpan.id = "memberSpan"+member.id;
+		memberSpan.innerHTML = member.name;
+		memberContainer.appendChild(memberSpan);
+	};
+
+	var addMember = function (name, observer) {
+		var MemberAction = {
+			action: "add",
+			name: name,
+			observer: observer,
+			vote: "",
+			clientKey: _clientKey,
+			roomKey: _roomKey
+		};
+		_socket.send(JSON.stringify(MemberAction));
+	};
+
+	var removeMember = function (element) {
+		var id = element;
+		var MemberAction = {
+			action: "remove",
+			id: id,
+			clientKey: _clientKey,
+			roomKey: _roomKey
+		};
+		_socket.send(JSON.stringify(MemberAction));
+	};
+	
+	init();
+	
+	return {
+
+		exitRoom : function (){
+			var serverId = sessionStorage.getItem('pointingpoker:serverid')*1;
+			if(serverId >= 0){
+				removeMember(serverId);
+			}
+		},
+		
+		selectVote : function (element, vote) {
+
+			var pointPickerContainer = document.getElementById('pointPickerContainer');
+			var iCount = pointPickerContainer.childNodes.length;
+			for(var i=0; i < iCount; i++){
+				pointPickerContainer.childNodes[i].className = "card";
+			}
+			element.className = "card selected";
+			var serverId = sessionStorage.getItem('pointingpoker:serverid')*1;
+			var MemberAction = {
+				action: "vote",
+				vote: vote,
+				id: serverId,
+				clientKey: _clientKey,
+				roomKey: _roomKey
+			};
+			_socket.send(JSON.stringify(MemberAction));
+		},
+
+		newGame : function () {
+			var MemberAction = {
+				action: "newgame",
+				clientKey: _clientKey,
+				roomKey: _roomKey
+			};
+			_socket.send(JSON.stringify(MemberAction));
+		},
+
+		showForm : function () {
+			document.getElementById("newGameContainer").style.display = "none";
+			document.getElementById("linkExit").style.display = "none";
+			document.getElementById("pointPickerContainer").style.display = "none";
+			document.getElementById("resultsContainer").style.display = "none";
+			document.getElementById("memberContainer").style.display = "none";
+			document.getElementById("roomEntryContainer").style.display = 'block';
+		},
+
+		hideForm : function (observer) {
+			if(!observer){
+				document.getElementById("newGameContainer").style.display = "block";
+				document.getElementById("pointPickerContainer").style.display = "block";
+			}
+			document.getElementById("linkExit").style.display = "block";
+			document.getElementById("resultsContainer").style.display = "block";
+			document.getElementById("memberContainer").style.display = "block";
+			document.getElementById("roomEntryContainer").style.display = "none";
+		},
+
+		formSubmit : function () {
+			var name = document.getElementById("inputName").value;
+			_roomKey = document.getElementById("inputRoomKey").value;
+			var observer = eval(document.getElementById("selectObserver").value);
+			if(name !== "" && _roomKey !== ""){
+				_clientKey = parseInt(Math.random()*1000000,10);
+				var wsPath = getWebSocketPath();
+				_socket = new WebSocket(wsPath+"/"+_clientKey+"/"+window.escape(_roomKey));
+				_socket.onmessage = onMessage;
+				_socket.onopen = function(){
+					localStorage.setItem('pointingpoker:roomkey', _roomKey);
+					sessionStorage.setItem('pointingpoker:clientkey', _clientKey);
+					localStorage.setItem('pointingpoker:username', name);
+					addMember(name, observer);
+				};
+			}
+		}
+	};
+}();
