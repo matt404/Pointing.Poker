66cf41361a217e1e2698c3f095a3615b4cbce56b
Matt Wilson <mwilson@mswis.com>
2016-09-04 17:18:47 -0700

    skldfj

--- server/app.js
+++ server/app.js
@@ -5,15 +5,26 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var members = {};
-var Member = {
+var rooms = {};
+var Member = function(){
+  var _member = {
     id: 0,
     name: "",
     observer: "",
     vote: "",
     clientKey: 0,
     roomKey: ""
+  }
+  return _member;
 };
 
+var Room = function(){
+  var _room = {
+    id: 0,
+    name: ""
+  };
+  return _room;
+};
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -83,47 +94,6 @@ io.on('connection', function (socket) {
       message: data
     });
     */
-  });
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
   });
 
   // when the user disconnects.. perform this
