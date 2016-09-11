46cb9fb66f1f51ba66d61ba3965c1e0512bc1b1a
Matt Wilson <mwilson@mswis.com>
2016-09-10 20:08:38 -0700

    slkdjf

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var redis = require('socket.io-redis');
+var redisIO = require('socket.io-redis');
+var redis = require('redis');
 var port = process.env.PORT || 3000;
 
-var members = {};
-var rooms = {};
+//var members = {};
+//var rooms = {};
 var Member = require('./app/models/Member');
 var Room = require('./app/models/Room');
 
 var redisHost = process.env.REDIS_ADDR || '127.0.0.1';
 var redisPort = process.env.REDIS_PORT || 6379;
 
-io.adapter(redis({ host: redisHost, port: redisPort }));
+var redisClient = redis.createClient(redisPort, redisHost);
+
+io.adapter(redisIO({ host: redisHost, port: redisPort }));
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -29,6 +32,20 @@ io.on('connection', function (socket) {
     var member = new Member(data);
     var roomKey = member.roomKey.toLowerCase();
     console.log('add',member);
+
+    redisClient.get("ppoker_members123", function (err, members) {
+
+        console.log("ppoker_members123", members);
+
+        if(members !== null && typeof(members) === "object"){
+          members[members.length] = member;
+        }else{
+          members = [ member ];
+        }
+
+        redisClient.set("ppoker_members123", members);
+
+    });
 
     socket.join(roomKey);
 
@@ -51,10 +68,18 @@ io.on('connection', function (socket) {
   socket.on('newgame', function (data) {
     console.log('newgame',data);
 
+    socket.emit('newgame', data);
+    socket.broadcast.emit('newgame', data);
+
   });
 
   socket.on('vote', function (data) {
     console.log('vote',data);
+
+    var member = new Member(data);
+
+    socket.emit('vote', member);
+    socket.broadcast.emit('vote', member);
 
   });
 
