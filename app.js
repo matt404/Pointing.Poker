fef216fbbc46b9a7c5b56fdfd4a57045a8954a51
Matt Wilson <mwilson@mswis.com>
2016-09-12 08:17:47 -0700

    stuff

--- app.js
+++ app.js
@@ -32,25 +32,34 @@ io.on('connection', function (socket) {
     var member = new Member(data);
     var roomKey = member.roomKey.toLowerCase();
     var roomCacheKey = "ppoker_room_"+roomKey;
-    //console.log('add',member);
+
+    console.log('add', data);
+
+    //redisClient.set(roomCacheKey, "[]");
 
     redisClient.get(roomCacheKey, function (err, val) {
 
         var members = [];
 
-        //console.log(roomCacheKey, err, val);
-
         if(val !== null && typeof(val) === "string" && val !== ""){
           members = JSON.parse(val);
         }
+        var memberCount = members.length;
+        for(var i=0; i < memberCount; i++){
+          var mbr = members[i];
+          if(mbr.name.toLowerCase() !== member.name.toLowerCase()){
+            socket.emit('add', mbr);
+          }
+        }
+
         members[members.length] = member;
 
         redisClient.set(roomCacheKey, JSON.stringify(members));
 
         socket.join(roomKey);
 
-        socket.emit('add', member);
-        socket.broadcast.emit('add', member);
+        io.to(roomKey).emit('add', member);
+        //socket.broadcast.to(roomKey).emit('add', member);
 
     });
 
@@ -75,18 +84,18 @@ io.on('connection', function (socket) {
 
   });
 
-  socket.on('vote', function (data) {
-    console.log('vote',data);
+  socket.on('vote', function (memberVote) {
+    console.log('vote',memberVote);
 
-    var member = new Member(data);
+    var roomKey = memberVote.roomKey.toLowerCase();
 
-    socket.emit('vote', member);
-    socket.broadcast.emit('vote', member);
+    io.to(roomKey).emit('vote', memberVote);
+    socket.broadcast.to(roomKey).emit('vote', memberVote);
 
   });
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-
+  socket.on('disconnect', function (n1, n2, n3) {
+    console.log('disconnect', n1, n2, n3);
   });
 });
